07486a6f0fad4534bbdba4e56c803111a89d4185
Fabian Schmauder <fabian.schmauder@gmail.com>
2022-10-28 16:59:32 +0200

    Add job endpoint

--- api-server/src/main.py
+++ api-server/src/main.py
@@ -2,6 +2,20 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 def health():
     return {"health": "OK"}
+
+
+@app.get("/job")
+def getJobs():
+    return [{
+        "id": "uid1",
+        "title": "Nice Job",
+        "description": "super nice job"
+    }, {
+        "id": "uid2",
+        "title": "Nice Job2",
+        "description": "super nice job2"
+    }]
