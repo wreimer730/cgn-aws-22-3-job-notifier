38a8d1116442b30c8aaed310cd872a132aa6755f
Fabian Schmauder <fabian.schmauder@gmail.com>
2022-10-31 18:00:29 +0100

    Add loadbalancer

--- infrastructure/webserver.tf
+++ infrastructure/webserver.tf
@@ -8,15 +8,7 @@ resource "aws_security_group" "allow_http" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  
-  ingress {
-    description = "Http"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    security_groups = [aws_security_group.allow_http_lb.id]
   }
 
   egress {
@@ -35,15 +27,13 @@ resource "aws_security_group" "allow_http" {
 resource "aws_instance" "webserver" {
   ami                         = "ami-08e2d37b6a0129927"
   instance_type               = "t3.micro"
-  subnet_id                   = aws_subnet.public_subnet.id
-  associate_public_ip_address = true
+  subnet_id                   = aws_subnet.private_subnet_b.id
+  associate_public_ip_address = false
   vpc_security_group_ids      = [aws_security_group.allow_http.id]
   key_name                    = "vockey"
   user_data                   = file("script/userdata.sh")
   iam_instance_profile        = "LabInstanceProfile"
-}
-
-output "webserver_ip" {
-  value       = aws_instance.webserver.public_ip
-  description = "Public ip address"
+  depends_on = [
+    aws_nat_gateway.nat
+  ]
 }
