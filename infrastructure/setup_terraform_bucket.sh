fd172f47027573872340da4306f6a9de0fdbfd86
wreimer730 <114474804+wreimer730@users.noreply.github.com>
2022-11-06 17:00:39 +0100

    Update tfstate bucketname

--- infrastructure/setup_terraform_bucket.sh
+++ infrastructure/setup_terraform_bucket.sh
@@ -1,4 +1,4 @@
-S3_BUCKET_NAME=terraform-state-12312d123f
+S3_BUCKET_NAME=terraform-state-20221106165959
 REGION=us-west-2
 if aws s3 ls "s3://$S3_BUCKET_NAME" 2>&1 | grep -q 'An error occurred'
 then
