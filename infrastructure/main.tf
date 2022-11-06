fa1d5e6a7faa6c9a910f8c4c43eb17d1fbdc471e
wreimer730 <114474804+wreimer730@users.noreply.github.com>
2022-11-06 17:02:32 +0100

    Update Bucket name

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -6,7 +6,7 @@ terraform {
     }
   }
    backend "s3" {
-    bucket = "terraform-state-12312d123f"
+    bucket = "terraform-state-20221106165959"
     key    = "terraform.tfstate"
     region = "us-west-2"
   }
